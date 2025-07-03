504fac85b87ca8e39f04061338372560dc8757ae
jccartwright <john.c.cartwright@comcast.net>
2025-07-03 14:38:36 -0600

    add ArcGIS support and simple WebMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useRef, useEffect } from "react";
+import Bookmarks from '@arcgis/core/widgets/Bookmarks';
+import Expand from '@arcgis/core/widgets/Expand';
+import MapView from "@arcgis/core/views/MapView";
+import WebMap from "@arcgis/core/WebMap";
+
 import './App.css'
 
+// Core API import
+import Graphic from "@arcgis/core/Graphic.js";
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    if (mapDiv.current) {
+      /**
+       * Initialize application
+       */
+      const webmap = new WebMap({
+        portalItem: {
+          id: "43b93cc9be994efbb5a60a2b2d85f151"
+        }
+      });
+
+      const view = new MapView({
+        container: mapDiv.current,
+        map: webmap
+      });
+
+      const bookmarks = new Bookmarks({
+        view
+      });
+
+      const bkExpand = new Expand({
+        view,
+        content: bookmarks,
+        expanded: true
+      });
+
+      // Add the widget to the top-right corner of the view
+      view.ui.add(bkExpand, "top-right");
+
+      // Bonus - how many bookmarks in the webmap?
+      view.when(() => {
+        if (webmap.bookmarks && webmap.bookmarks.length) {
+          console.log("Bookmarks: ", webmap.bookmarks.length)
+        } else {
+          console.log("No bookmarks in this webmap.")
+        }
+      })
+    }
+  }, [mapDiv])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <div className="mapDiv" ref={mapDiv}></div>
   )
 }
 
