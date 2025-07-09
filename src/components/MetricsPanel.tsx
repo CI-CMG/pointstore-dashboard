00f8a62163a70d955a3d3457d34b76f8618c5b0a
jccartwright <john.c.cartwright@comcast.net>
2025-07-08 19:37:51 -0600

    minor type fix

--- src/components/MetricsPanel.tsx
+++ src/components/MetricsPanel.tsx
@@ -13,7 +13,7 @@ export default function MetricsPanel({data}: AppProps) {
       { data ?
         <>
         <p>Report Date: <span className='emphasis'>{data['report_date']}</span></p>
-        <p>Total Count: <span className='emphasis'>{parseInt(data['record_count']).toLocaleString("en-US")}</span></p>
+        <p>Total Count: <span className='emphasis'>{data['record_count'].toLocaleString("en-US")}</span></p>
         <p>Archive Dates:<br/><span className='emphasis'>{data['min_entry_date']} to {data['max_entry_date']}</span></p>
         <p><span className='emphasis'>{data.order_count.count}</span> orders in the last 30 days</p>
         </>
